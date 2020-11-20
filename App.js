093045071d15ea86fb2ee44c99b2bf8b2440a7e2
Alexandre Reveilleau dos Reis <alexandrerreis@gmail.com>
2020-11-20 13:06:40 -0300

    removido logs de warning

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { View, StatusBar } from 'react-native';
+import { View, StatusBar, LogBox } from 'react-native';
 
 import AuthProvider from './src/contexts/auth';
 
 import Routes from './src/routes/index';
+
+LogBox.ignoreAllLogs();
 
 export default function App() {
   return (
