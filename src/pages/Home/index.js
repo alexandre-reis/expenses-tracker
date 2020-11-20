76e0022cef06dc602ef8d59d442c414ba8b34840
Alexandre Reveilleau dos Reis <alexandrerreis@gmail.com>
2020-11-19 22:46:00 -0300

    add perfil, add despesa receita

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,18 +1,41 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { Button, Text, View } from 'react-native';
 import { AuthContext } from '../../contexts/auth';
 
-// import { Container } from './styles';
+import Header from '../../components/Header';
+import HistoricoList from '../../components/HistoricoList';
+
+import { Background, Container, List, Nome, Saldo, Title } from './styles';
 
 export default function Home() {
   const { user, signOut } = useContext(AuthContext);
 
+  const [historico, setHistorico] = useState([
+    { key: '1', tipo: 'receita', valor: 1200 },
+    { key: '2', tipo: 'despesa', valor: 200 },
+    { key: '3', tipo: 'receita', valor: 40 },
+    { key: '4', tipo: 'receita', valor: 89.62 },
+    { key: '5', tipo: 'despesa', valor: 500 },
+    { key: '6', tipo: 'despesa', valor: 310 },
+  ]);
+
   return (
-    <View>
-      <Text>Home</Text>
-      <Text>{user && user.nome}</Text>
-      <Text>{user && user.email}</Text>
-      <Button title="Sair da conta" onPress={() => signOut()} />
-    </View>
+    <Background>
+      <Header />
+      <Container>
+        <Nome>{user && user.nome}</Nome>
+
+        <Saldo>R$ 123,00</Saldo>
+      </Container>
+
+      <Title>Histórico</Title>
+
+      <List
+        showsVerticalScrollIndicator={false}
+        data={historico}
+        keyExtractor={(item) => item.key}
+        renderItem={({ item }) => <HistoricoList data={item} />}
+      />
+    </Background>
   );
 }
